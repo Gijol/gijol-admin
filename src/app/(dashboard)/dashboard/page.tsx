0fc785a58700563165a43f4e082603b3d0ce74ba
Dongho Seo <ehcws333@gmail.com>
2023-10-01 15:40:59 +0900

    chore: delete unused date picker component

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -46,10 +46,6 @@ export default function DashboardPage() {
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-            <div className="flex items-center space-x-2">
-              <DateRangePicker />
-              <Button>Download</Button>
-            </div>
           </div>
           <Tabs defaultValue="users" className="space-y-4">
             <TabsList>
