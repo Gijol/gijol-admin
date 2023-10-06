b180aa56a1f4077a4642b4d900a65c91bd596cff
Dongho Seo <ehcws333@gmail.com>
2023-10-06 22:10:20 +0900

    chore: change to table form with using tremor

--- src/components/course-content.tsx
+++ src/components/course-content.tsx
@@ -1,19 +1,48 @@
 import { getUserAvgData } from "@/lib/const/user-avg-data";
+import {
+  Card,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeaderCell,
+  TableBody,
+  TableCell,
+  Text,
+  Badge,
+} from "@tremor/react";
 
 export async function CourseContent() {
   const data = await getUserAvgData();
   return (
-    <div>
-      {data.map((course, idx) => (
-        <div key={idx}>
-          <div>
-            <span>{course.course_code}</span>
-            <span>{course.course_name}</span>
-            <span>{course.year}</span>
-          </div>
-          <div>평균 성적 : {course.average_grade}</div>
-        </div>
-      ))}
-    </div>
+    <Card>
+      <Table className="mt-5">
+        <TableHead>
+          <TableRow>
+            <TableHeaderCell>Code</TableHeaderCell>
+            <TableHeaderCell>Name</TableHeaderCell>
+            <TableHeaderCell>Year</TableHeaderCell>
+            <TableHeaderCell>Semester</TableHeaderCell>
+            <TableHeaderCell>Avg Grade</TableHeaderCell>
+            <TableHeaderCell>Number of Students</TableHeaderCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((item) => (
+            <TableRow key={item.course_name}>
+              <TableCell>{item.course_code}</TableCell>
+              <TableCell>
+                <Text>{item.course_name}</Text>
+              </TableCell>
+              <TableCell>{item.year}</TableCell>
+              <TableCell>{item.semester}</TableCell>
+              <TableCell>
+                <Badge color="emerald">{item.average_grade?.toFixed(2)}</Badge>
+              </TableCell>
+              <TableCell>{item.num_students}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Card>
   );
 }
